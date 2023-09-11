898639fea9028f5cef1d0b8f2f98906f59ef0df3
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2023-09-11 16:45:01 -0500

    ADD: Route to manage users

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,4 @@ import { UsersController } from '../controllers/users.js'
 
 export const usersRouter = Router()
 
-usersRouter.get('/', (req, res) => {
-  res.json(UsersController.getAll())
-})
+usersRouter.get('/', UsersController.getAll)
