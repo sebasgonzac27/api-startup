359f56f25a9a77d9b95fabb9c1a3035057abaaa9
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2023-09-13 10:48:04 -0500

    UPDATE: Methods names

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,5 @@
 import { UserModel } from '../models/users.js'
+import { validatePartialUser, validateUser } from '../schemas/user.js'
 import { error, success } from '../utils/responses.js'
 
 export class UsersController {
@@ -21,31 +22,39 @@ export class UsersController {
     }
   }
 
-  static async addUser (req, res) {
-    const data = req.body
-    try {
-      const added = await UserModel.addUser(data)
-      success(req, res, added, 200)
-    } catch (e) {
-      error(req, res, e.message, e.status)
+  static async create (req, res) {
+    const result = validateUser(req.body)
+    if (result.success) {
+      try {
+        const added = await UserModel.create(result.data)
+        success(req, res, added, 200)
+      } catch (e) {
+        error(req, res, e.message, e.status)
+      }
+    } else {
+      error(req, res, JSON.parse(result.error.message), 400)
     }
   }
 
-  static async updateUser (req, res) {
-    const data = req.body
+  static async update (req, res) {
+    const result = validatePartialUser(req.body)
     const { id } = req.params
-    try {
-      const updated = await UserModel.updateUser(data, id)
-      success(req, res, updated, 200)
-    } catch (e) {
-      error(req, res, e.message, e.status)
+    if (result.success) {
+      try {
+        const updated = await UserModel.update(result.data, id)
+        success(req, res, updated, 200)
+      } catch (e) {
+        error(req, res, e.message, e.status)
+      }
+    } else {
+      error(req, res, JSON.parse(result.error.message), 400)
     }
   }
 
-  static async deleteUser (req, res) {
+  static async delete (req, res) {
     const { id } = req.params
     try {
-      const deleted = await UserModel.deleteUser(id)
+      const deleted = await UserModel.delete(id)
       success(req, res, deleted, 200)
     } catch (e) {
       error(req, res, e.message, e.status)
