9df68d454c0cb0aa925524d32aaca63e5ffad39c
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2023-09-13 10:47:43 -0500

    UPDATE: Routes to api requests from campuses

--- src/routes/campuses.js
+++ src/routes/campuses.js
@@ -1,7 +1,17 @@
 import { Router } from 'express'
+import { CampusesController } from '../controllers/campuses.js'
 
 export const campusesRouter = Router()
 
-campusesRouter.get('/', (req, res) => {
-  res.json({ title: 'campuses' })
-})
+// GET
+campusesRouter.get('/', CampusesController.getAll)
+campusesRouter.get('/:id', CampusesController.getById)
+
+// POST
+campusesRouter.post('/', CampusesController.create)
+
+// PATCH
+campusesRouter.patch('/:id', CampusesController.update)
+
+// DELETE
+campusesRouter.delete('/:id', CampusesController.delete)
